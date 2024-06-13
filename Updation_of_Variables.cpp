6e6e773de3e2252b7c5cf33a31571bd39e7b0d85
Amirul898 <amirul898amir@gmail.com>
2024-06-13 20:17:00 +0600

    update code

---
+++ Updation_of_Variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std ;
+int main (){
+
+int x = 7;
+cout<<x<<endl; //7
+x = x + 6;
+cout<<x<<endl; //13
+x = x - 10;
+cout<<x<<endl; //3 
+
+
+}
---
+++ Variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std ;
+int main (){
+  int a ; // Variable diclaration
+  a = 5;  // Variable  initialization
+  cout << a  << endl;
+
+  int b =34;
+  cout << b << endl; //Variable diclaration and Variable  initialization
+
+
+
+  int x, y ; //diclaration
+  x = 234;  //initialization
+  y = 348;  //initialization
+  cout << x+y <<endl ;  //print
+
+}
