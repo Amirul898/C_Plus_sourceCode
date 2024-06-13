b9cd8a64e6dbe0ab38a5be4a212279658517e57a
Amirul898 <amirul898amir@gmail.com>
2024-06-13 23:48:09 +0600

    input code

--- input.cpp
+++ input.cpp
@@ -2,4 +2,14 @@
 using namespace std;
 int main (){
 
+int a ,b , add;
+
+cout << "type Your first number : " ;
+cin >> a;  //input 
+cout << "type Your secend number : " ;
+cin >> b; //input 
+
+add = a+b;
+cout << "sum is : " << add; 
+
 }
