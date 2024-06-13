bcb23658463bb016833f8928ae5f01223a3afff0
Amirul898 <amirul898amir@gmail.com>
2024-06-13 19:32:01 +0600

    next line code

---
+++ NextLine.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main (){
+  cout  << "hello World ";
+  cout << endl; //new line 
+  cout << "HEllO BANGLADASH";
+  cout << endl; //new line 
+
+
+
+  cout << "first Line " << endl;
+  cout << "secend Line "; 
+
+
+  cout << endl; //new line 
+  
+  cout << "first Line " << endl << "secend Line "; 
+
+}
